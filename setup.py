c8412f2ee5efa21ff56b00f05f5de9bd788c9f63
Cd Chen <cdchen@nicestudio.com.tw>
2014-08-06 22:27:50 +0800

    Release 0.1

--- setup.py
+++ setup.py
@@ -56,10 +56,6 @@ setup(
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: MIT License',
                  'Operating System :: OS Independent',
-                 'Programming Language:: Python:: 2.5',
-                 'Programming Language:: Python:: 2.6',
-                 'Programming Language:: Python:: 2.7',
                  'Programming Language :: Python',
-                 'Programming Language :: Python :: 3',
                  'Topic :: Utilities'],
 )
